44d8288ed3894baa30f25d6c09721204a7b3e28c
chrislupdx <christopherlu.pdx@gmail.com>
2018-07-28 19:20:36 -0700

    list reverse push before w/ galen (still broken)

--- Js/cipher.js
+++ Js/cipher.js
@@ -1,28 +1,47 @@
 let english = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25}
 let rot = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r','f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k', 'y':'l', 'z':'m'}
-let querent = prompt("Input?").toLowerCase();
+let coded = prompt("coded message??").toLowerCase();
+// abcdefgh= nopqrstu
 
+let reversed = [];
+function array_flip( trans )
+
+var key
 let reversed = {}
 for (key in rot) {
-    let value = rot[key]
-    // console.log(reversed)
-  }
+  reversed[rot[key]] = key
+  console.log(key)
+  return reversed
+}
 
-// measure/translate/pull by indexical length?
+let decoded = []
+for (let i = 0; i< coded.length; i++) {
+  char = coded[i]
+  rotd = reversed[char]
+  decoded.push(rotd)
+  // decoded.push(ret[decoded[i]])
+  let joined = decoded.join('')
+  console.log(joined)
+}
+
+function swap(rot){
+  var ret = [];
+  for(var key in rot){
+    ret[rot[key]] = key;
+  }
+  return ret;
+  reversed.push(ret)
+}
+
+reversed.push(something[rot[i]])
+}
 
 let encoded = []
 for (let i = 0; i < querent.length; i++) {
-  // char = querent[i]
-  // rotd = rot[char]
-  // encoded.push(rotd)
+  char = querent[i]
+  rotd = rot[char]
+  encoded.push(rotd) // lines 13-15=16
   encoded.push(rot[querent[i]])
-  // lines 15-17=18
-}
   let joined = encoded.join('')
 console.log(joined)
-
-
-let decoded = {}
-for (let i = 0; i< querent.length; i++) {
-  console.log(querent[i]);
 }
